8dcb8eb33a6a797c351c66a39eafc01a521cb7b7
Raian Hasan <mremoz08@live.com>
2023-01-10 02:40:09 +0600

    login done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="card">
+  <div class="card-header">
+    Footer 
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p> this is foter .</p>
+      <footer class="blockquote-footer"></footer>
+    </blockquote>
+  </div>
+</div>
